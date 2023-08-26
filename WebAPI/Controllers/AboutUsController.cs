62023fa587ea8784f7f656599bf5645fa1f2437a
CyberObservers <13816750292@163.com>
2023-08-26 22:52:11 +0800

    add webapi of customer_service and about_us

---
+++ WebAPI/Controllers/AboutUsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Nodes;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Repair.Models;
+using Repair.Server;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AboutUsController : Controller
+    {
+        [HttpGet("CustomerService")]
+        public IEnumerable<CustomerService> GetUserInfo()
+        {
+            List<CustomerService> customerServices = CustomerServiceServer.Query();
+            //Console.WriteLine(uid);
+            return customerServices;
+        }
+    }
+}
---
+++ WebAPI/Controllers/CustomerServiceController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repair.Models;
+using Repair.Server;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerServiceController : Controller
+    {
+        [HttpGet("AboutUs")]
+        public IEnumerable<AboutUs> GetUserInfo()
+        {
+            List<AboutUs> aboutUs = AboutUsServer.Query();
+            //Console.WriteLine(uid);
+            return aboutUs;
+        }
+    }
+}
